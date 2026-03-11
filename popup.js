bbea275856aa0b3c4a8f0f2b5e5dd884e1948fce
elishamutang <elishamutang@outlook.com>
2026-03-11 23:48:11 +0800

    update: Completed styling and function of extension's popup.

--- popup.js
+++ popup.js
@@ -61,6 +61,61 @@ document.getElementById("logout").addEventListener("click", async (e) => {
   }
 });
 
+// Form submission
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("job-preview-form")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      // Get user auth token
+      const token = await chrome.storage.local.get("authToken");
+
+      // Construct FormData for submission
+      const formData = new FormData(
+        document.getElementById("job-preview-form"),
+      );
+
+      // Show loading, disable form
+      const form = e.target;
+      const overlay = document.getElementById("loading-overlay");
+      const confirmBtn = document.getElementById("confirmBtn");
+
+      form.classList.add("loading");
+      overlay.classList.remove("loading-hidden");
+      confirmBtn.textContent = "Saving...";
+
+      try {
+        const response = await fetch("http://localhost:8000/api/create", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token.authToken}`,
+            Accept: "application/json",
+          },
+          body: formData,
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+          chrome.storage.local.remove("currentJob");
+          document.getElementById("job-preview-form").style.display = "none";
+          document.getElementById("job-preview-form").reset();
+
+          document.getElementById("status").textContent = data.message;
+        }
+      } catch (error) {
+        console.log(error);
+        document.getElementById("status").textContent =
+          "Add failed - something went wrong.";
+      } finally {
+        form.classList.remove("loading");
+        overlay.classList.add("loading-hidden");
+        confirmBtn.textContent = "Confirm";
+      }
+    });
+});
+
 // Check login status on load
 chrome.storage.local.get(["authToken"], (result) => {
   if (result.authToken) {
@@ -72,20 +127,26 @@ chrome.storage.local.get(["authToken"], (result) => {
         console.log("Loaded job: ", result.currentJob);
 
         // Title
-        document.getElementById("job-title").textContent =
+        document.getElementById("job-title-text").textContent =
           result.currentJob.title;
+        document.getElementById("job-title").value = result.currentJob.title;
 
         // Company
-        document.getElementById("job-company").textContent =
+        document.getElementById("job-company-text").textContent =
+          result.currentJob.company;
+        document.getElementById("job-company").value =
           result.currentJob.company;
 
         // Industry
-        document.getElementById("job-industry").textContent =
+        document.getElementById("job-industry-text").textContent =
+          result.currentJob.industry;
+        document.getElementById("job-industry").value =
           result.currentJob.industry;
 
         // Type
-        document.getElementById("job-type").textContent =
+        document.getElementById("job-type-text").textContent =
           result.currentJob.type;
+        document.getElementById("job-type").value = result.currentJob.type;
 
         // Location
         chrome.storage.local.get(["countries"], (result) => {
@@ -96,13 +157,22 @@ chrome.storage.local.get(["authToken"], (result) => {
 
             result.countries.forEach((country) => {
               const option = document.createElement("option");
-              option.value = country.name;
+              option.value = country.id;
               option.textContent = country.name;
 
               locationElem.append(option);
             });
           }
         });
+
+        // Advertised Salary
+        document.getElementById("advertised-salary-text").textContent =
+          result.currentJob.salary ?? "N/A";
+        document.getElementById("advertised_salary").value =
+          result.currentJob.salary;
+
+        // URL
+        document.getElementById("job_link").value = result.currentJob.url;
 
         // Preview
         document.getElementById("job-preview-form").style.display = "block";
